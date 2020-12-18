d3e371f63f61f499569a1e4ecde1ab91d6bdf5c9
tulbadex <ibrahimadedayo@rocketmail.com>
2020-12-18 17:14:58 +0100

    Added custom config settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#FF6363'
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
       }
     },
   },
